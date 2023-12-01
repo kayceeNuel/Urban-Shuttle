66dcceb31f73f4c8be86717cefb9bfad24c795ba
Kaycee Nuel <heykayceee@gmail.com>
2023-12-01 19:05:54 +0100

    login Updated

--- Backend/controllers/Authcontroller.js
+++ Backend/controllers/Authcontroller.js
@@ -65,6 +65,42 @@ const AuthController = {
         }
     },
 
+    login : async (req, res) => {
+        
+        try {
+            
+            const {email , password}= req.body;
+
+            if (!email || email.trim() === '') {
+                return res.status(400).json ({error: 'Username and email is required'});
+            }
+
+            if (!password || password.trim() === '') {
+                return res.status(400).json ({error: 'Password is required'})
+            }
+
+            const user = await userModel.findOne( {
+                $or: [{email}, {password}]
+            })
+
+            if (!user) {
+                return res.status(400).json ({error: 'User not found'})
+            };
+
+            const passwordMatched = bcrypt.compare(password, user.password)
+            if (!passwordMatched) {
+                return res.status(401).json({error: 'Incorrect password'});
+            };
+            const token = jwt.sign({userid: user._id}, Secret_Key, {expiresIn: '1000h'});
+            res.status(200).json({message: 'Login successful', token, user});
+
+
+        }  catch (error){
+            console.error(error);
+            res.status(501).json ({error: 'Internal server error'});
+        }
+    },
+
 };
 
     module.exports = AuthController;
