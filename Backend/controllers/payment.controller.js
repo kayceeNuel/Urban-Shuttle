56071162cb979f96fe8058e96a4b9a7467e7bcd3
Kaycee Nuel <heykayceee@gmail.com>
2023-12-16 23:20:00 +0100

    updating the payment gateway

--- Backend/controllers/payment.controller.js
+++ Backend/controllers/payment.controller.js
@@ -1,10 +1,9 @@
 
 
+const stripe = require('stripe');
+const stripe_secret_key = process.env.STRIPE_SECRET_KEY; 
+const stripe_public_key = process.env.STRIPE_PUBLIC_KEY ;
 
-const paymentController = {
-    payment_checkout: async(req,res) => {
 
-    }
-}
 
 module.exports = paymentController;
