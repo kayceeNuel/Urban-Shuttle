32a54efa1f124c5e5fdb8995a5473bce0d5ff0d1
Kaycee Nuel <heykayceee@gmail.com>
2023-12-20 15:19:52 +0100

    update

--- Backend/controllers/payment.controller.js
+++ Backend/controllers/payment.controller.js
@@ -6,30 +6,27 @@ const stripe = require('stripe')(stripe_key);
 
 const paymentController = {
 
-    // createNewCustomer: async (req, res)  => {
-    //     try{ 
-    //         const customer = await stripe.customers.create({
-    //             name : req.body.name , 
-    //             email: req.body.name,
-    //         }); 
-    //         res.status(200).send(customer)
-    //     } catch(error) {
-    //         console.error(error); 
-    //         res.status(501).json({ 
-    //             error:  'internal server error'
-    //         })
-    //     }
-    // }
+    createNewCustomer: async (req, res) => {
+        try {
+            const customer = await stripe.customers.create({
+                name: req.body.name,
+                email: req.body.email,
+            });
+            res.status(200).json(customer);
+        } catch (error) {
+            console.error('Error creating Card', error);
+            res.status(500).json({ error: 'Something went wrong on Stripe`s end. (These are rare.)' });
+        }
+    },
 
     addCustomersCard: async (req, res) => {
 
-        
-            const {
+        const {
                 customer_id, 
                 card_Name, 
                 card_ExpYear, 
                 card_ExpMonth, 
-                card_Numerb, 
+                card_Number, 
                 card_CVC,
             } = req.body;
 
@@ -37,14 +34,20 @@ const paymentController = {
                 const card_Token = await stripe.tokens.create({
                     card: {
                         name: card_Name, 
-                        number: card_Numerb, 
+                        number: card_Number, 
                         exp_Month: card_ExpMonth,
                         exp_year: card_ExpYear, 
                         CVC: card_CVC
                     },
                 });
-                const addingCustomerCard = await stripe.customers.createSource
+                const addingCustomerCard = await stripe.customers.createSource(customer_id, {
+                    source: card_Token.id,
+                });
+                return res.status(200).json(addingCustomerCard);
+            } catch(error){
+                console.error('error creating card:', error);
+                res.status(500).json({error: 'Something went wrong on Stripe’s end. (These are rare.)'})
             }
         
     }
-}
+ }
