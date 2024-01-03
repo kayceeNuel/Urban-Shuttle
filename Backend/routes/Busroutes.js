638b559611a9c8e8daa24c005a9feb6132ec34ba
Kaycee Nuel <heykayceee@gmail.com>
2024-01-03 13:54:30 +0100

    Updating Bus routes

--- Backend/routes/Busroutes.js
+++ Backend/routes/Busroutes.js
@@ -4,21 +4,27 @@ const express = require('express');
 const BusController = require('../controllers/Buscontroller');
 const BusRouter = express.Router();
 
-
-BusRouter.post('/reservation', (req, res) =>{
-    BusController.getreservation( req,res);
+ 
+BusController.post('/create', (req,res) => {
+    BusController.create(req,res);
 });
 
-BusRouter.get('/Available Bus', (req,res) => {
-    BusController.getAvailableBus(req, res);
-}); 
+BusController.get('/get', (req,res) => {
+    BusController.getBuses(req,res);
+});
 
-BusRouter.post('/Book Seat', (req, res) => {
-    BusController.Book_Seat(req, res);
-}); 
+BusController.get('/id', (req,res) => {
+    BusController.getBusId(req,res);
+});
 
 
+BusController.put('/:id', (req,res) => {
+    BusController.updateBusId(req,res);
+});
 
+BusController.delete('/id', (req,res) => {
+    BusController.deleteBusId(req,res)
+});
 
 
 
