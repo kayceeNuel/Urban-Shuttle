c7a691786c9dfd6b9304c00d2125aad6119911d0
Kaycee Nuel <heykayceee@gmail.com>
2024-01-04 15:14:24 +0100

    updated schema

--- Backend/model/passenger.model.js
+++ Backend/model/passenger.model.js
@@ -1,11 +1,26 @@
 
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose');
 
 const passengersSchema = new mongoose.Schema({
-    Name: String, 
-    email: String, 
-    phoneNumber: Number,
-})
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32,
+    },
+    
+    email: {
+        type: String,
+        required: false,
+    },
 
-const passengersModel = mongoose.model('passenger', passengersSchema)
-module.exports = passengersModel
+    phone: {
+        type: String,  
+        required: true,
+        maxlength: 10,  
+        minlength: 10,  
+    },
+});
+
+const passengersModel = mongoose.model('passenger', passengersSchema);
+module.exports = passengersModel;
