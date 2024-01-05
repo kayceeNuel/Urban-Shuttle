a53587f7e7441ebf87b0158f48078f632037f363
Kaycee Nuel <heykayceee@gmail.com>
2024-01-05 12:20:47 +0100

    changed HTTP method

--- Backend/routes/Passenger.routes.js
+++ Backend/routes/Passenger.routes.js
@@ -5,7 +5,7 @@ const passengerController = require('../controllers/passenger.controller');
 const passengerRouter = express.Router();
 
 
-passengerRouter.post('/Details', (req,res) =>{
+passengerRouter.get('/Details', (req,res) =>{
     passengerController.passengersInfo(req,res)
 })
 
